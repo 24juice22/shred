75bde7f5f70ce96bd487c41457ddbff4e326ab91
24juice22 <mike.james.nunn@gmail.com>
2023-03-09 14:40:47 -0500

    AddWorkoutModal Functionality
    add functionality to AddWorkoutModal Component. Can now take in inputs and display them in a list when submitted.

--- src/components/AddWorkoutModal.js
+++ src/components/AddWorkoutModal.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExitButton from './ExitButton';
+import Workout from './Workout';
 
 function AddWorkoutModal({ addWorkoutModalVisible, setAddWorkoutModalVisible, setModal }) {
+  const [workoutList, setWorkoutList] = useState([]);
+  const [workout, setWorkout] = useState({exercise: "", calories: ""});
 
   const workoutModalStyle = {
     visibility: addWorkoutModalVisible ? "visible" : "hidden",
     transform: addWorkoutModalVisible ? "scale(1)" : "scale(0.1)"
-  }  
+  }
+  
+  function handleChange(e) {
+    setWorkout(prevWorkout => {
+      return {
+        ...prevWorkout,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  function handleAdd(e) {
+    e.preventDefault();
+    setWorkoutList(prevList => {
+      return [...prevList, workout]
+    })
+  }
+
+  const displayWorkoutList = workoutList.map((workout,index) => 
+    <Workout exercise={workout.exercise} calories={workout.calories} key={index}/>
+  )
 
   return (
     <div className="modal" style={workoutModalStyle}>
-        <ExitButton setVisibility={setAddWorkoutModalVisible} setModal={setModal} />    
-        <h2 className="modal__title">Workouts</h2>
+        <ExitButton setVisibility={setAddWorkoutModalVisible} setModal={setModal}/>    
+        <h2 className="modal__title modal__title-primary">Workouts</h2>
+        <form className="modal__form container__wide" onSubmit={handleAdd}>
+            <div className="modal__inputs">
+                <input 
+                    type="text"
+                    placeholder="Exercise"
+                    value={workout.exercise}
+                    onChange={handleChange}
+                    name="exercise"
+                />
+            </div>
+            <div className="modal__inputs">
+                <input
+                    type="text"
+                    placeholder="Calories"
+                    value={workout.calories}
+                    onChange={handleChange}
+                    name="calories"
+                />
+            </div>
+            <button className="btn modal__btn">Add</button>
+        </form>
+        <div className="modal__list modal__list--workouts">
+          {displayWorkoutList}
+        </div>
     </div>
   )
 }
