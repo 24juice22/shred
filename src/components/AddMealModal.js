0275d1619a097eb900aa3b1ae4965231b044c639
24juice22 <mike.james.nunn@gmail.com>
2023-03-09 23:56:58 -0500

    Simplify AddMealModal Component
    simplify component by making it comprised of other components

--- src/components/AddMealModal.js
+++ src/components/AddMealModal.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExitButton from './ExitButton';
+import MealForm from './MealForm';
+import MealList from './MealList';
+import MealTotal from './MealTotal';
 
 function AddMealModal({ addMealModalVisible, setAddMealModalVisible, setModal }) {
-  
+  const [mealList, setMealList] = useState([]);
+
   const mealModalStyle = {
     visibility: addMealModalVisible ? "visible" : "hidden",
     transform: addMealModalVisible ? "scale(1)" : "scale(0.1)"
@@ -12,6 +16,9 @@ function AddMealModal({ addMealModalVisible, setAddMealModalVisible, setModal })
     <div className="modal modal--primary" style={mealModalStyle}>
         <ExitButton setVisibility={setAddMealModalVisible} setModal={setModal}/>    
         <h2 className="modal__title">Meals</h2>
+        <MealForm setMealList={setMealList}/>
+        <MealList mealList={mealList} />
+        <MealTotal mealList={mealList}/>
     </div>
   )
 }
