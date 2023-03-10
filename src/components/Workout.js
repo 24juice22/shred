ee20cdcd32b0868377cd176dfcd48a46dde7c2e3
24juice22 <mike.james.nunn@gmail.com>
2023-03-09 22:46:28 -0500

    Update Workout HTML
    Update Workout HTML

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 function Workout({ exercise, calories}) {
   return (
-    <div className="container--small">
-        <div className="workout">
-            <p className="workout__text">{exercise}</p>
-            <p className="workout__text">{calories} Cal</p>
-        </div>
+    <div className="workout">
+        <p className="workout__text">{exercise}</p>
+        <p className="workout__text">{calories} Cal</p>
     </div>
   )
 }
