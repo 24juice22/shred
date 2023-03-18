afa8d9b5504d6391bd588426c8a70900680c27fa
24juice22 <mike.james.nunn@gmail.com>
2023-03-18 13:40:43 -0400

    Rename TDEEDisplay to TodaysData
    Rename TDEEDisplay to TodaysData

---
+++ src/components/TodaysData.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './Card'
+
+function TodaysData({ tdeeVisible, tdee, dailyData }) {
+
+    const todaysDataStyle = {
+        visibility: tdeeVisible ? "visible" : "hidden",
+    }
+
+  return (
+    <div className="tdee" style={todaysDataStyle}>
+        <h3>Today's Snapshot</h3>
+        <Card dailyData={dailyData} tdee={tdee}/>
+    </div>
+  )
+}
+
+export default TodaysData
