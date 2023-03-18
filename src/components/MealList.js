edd3ee729fa8f969b56c7172742670aee45df4b6
24juice22 <mike.james.nunn@gmail.com>
2023-03-17 23:57:22 -0400

    Increase Container Width for MealList
    Increase container width for MealList

--- src/components/MealList.js
+++ src/components/MealList.js
@@ -3,7 +3,7 @@ import Meal from './Meal'
 
 function MealList( { mealList }) {
   return (
-    <div className="modal__list container">
+    <div className="modal__list container--wide">
         {mealList.map((meal, index) => {
             return (
                 <Meal meal={meal.meal} calories={meal.calories} protein={meal.protein} key={index}/>
