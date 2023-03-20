a528622f2f97aa2dcba0f31cc3c78bc0f82237fb
24juice22 <mike.james.nunn@gmail.com>
2023-03-20 15:19:40 -0400

    Update Card Component
    Rearrange elements in card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
-function Card({ tdee, dailyData }) {
+function Card({ dailyData }) {
   return (
     <div className="card">
-        <p>Calories Eaten: {dailyData.mealCalories}</p>
-        <p>-</p>
-        <p>Calories Burned: {dailyData.workoutCalories}</p> 
-        <p>-</p>
-        <p>TDEE: {tdee}</p>
+        <p className="card__text">TDEE: {dailyData.tdee} Cal</p>
+        <p className="card__text">Calories Eaten: {dailyData.mealCalories} Cal</p>
+        <p className="card__text">Calories Burned: {dailyData.workoutCalories} Cal</p> 
+        <div className="card__totals">
+            <p className="card__total">Deficit: {dailyData.tdee + dailyData.workoutCalories - dailyData.mealCalories} Cal</p>
+            <p className="card__total">Protein: {dailyData.protein} g</p>
+        </div>
     </div>
   )
 }
